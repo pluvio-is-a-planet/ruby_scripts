0da74fc06b22f3d31a4ad5135b1f0a49c214edd4
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-02 21:30:19 +0200

    fix: caesar_cipher now properly stays alphabetical
    In some cases, specifically with capital letters, if the difference
    between 90 and the ascii decimal value of a character in a string was
    less than 26, the character would get translated incorrectly. Method
    now properly ensures that the translation stays within the range of
    0-25, as in the caesar cipher arithmetic.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,12 +1,10 @@
 def caesar_cipher(string, shift_factor)
   cipher = string.split('')
   cipher = cipher.map do |char|
+    base = char.ord < 91 ? 65 : 97
     int = char.ord
     if int.between?(65, 90) || int.between?(97, 122)
-      shifted_int = int + shift_factor
-      unless shifted_int.between?(65, 90) || shifted_int.between?(97, 122)
-        shifted_int = shifted_int - 26
-      end
+      shifted_int = (((int - base) + shift_factor) % 26) + base
       shifted_int
     else
       int
